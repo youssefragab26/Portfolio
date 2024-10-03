894fbc170129ab1c176330102321146f8ba8f6b0
Youssefragab26 <youssef26ragab@gmail.com>
2024-10-03 13:14:13 +0300

    update

---
+++ Home.js
@@ -0,0 +1,11 @@
+let AllItems = document.getElementById("All-Items");
+let PP = document.getElementById("PP");
+let info = document.getElementById("info");
+let buttons = document.getElementById("buttons");
+let About = document.getElementById("About");
+let Skills = document.getElementById("Skills");
+let Education = document.getElementById("Education");
+let Project = document.getElementById("Project");
+let Contact = document.getElementById("Contact");
+let Github = document.getElementById("Github"); 
+
