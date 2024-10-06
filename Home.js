11d36c1873c445549bd1a3346d994539f810b512
Youssefragab26 <youssef26ragab@gmail.com>
2024-10-06 15:13:23 +0300

    update

--- Home.js
+++ Home.js
@@ -1,11 +1,6 @@
-let AllItems = document.getElementById("All-Items");
-let PP = document.getElementById("PP");
-let info = document.getElementById("info");
-let buttons = document.getElementById("buttons");
-let About = document.getElementById("About");
-let Skills = document.getElementById("Skills");
-let Education = document.getElementById("Education");
-let Project = document.getElementById("Project");
-let Contact = document.getElementById("Contact");
-let Github = document.getElementById("Github"); 
+const Skills = document.getElementById("Skills");
 
+Skills.onclick = function () {
+    document.body.style.background = 'red';
+
+}
